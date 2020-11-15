cce3f701527587a66ddef6889c20f0e85f4cf751
Toolkit User <issam.laradji@gmail.com>
2020-11-15 15:29:59 +0000

    added train_on_loader

--- README.md
+++ README.md
@@ -19,49 +19,50 @@ pip install --upgrade --quiet git+https://github.com/haven-ai/haven-ai
 ### 1. Create trainval.py with the following code
 
 ```python
-import tqdm, os
+import tqdm
+import os
 
 from haven import haven_examples as he
 from haven import haven_wizard as hw
 
-# 0. define trainval function
+# 0. define a list of experiments
+EXP_LIST = [{'dataset':'syn', 'model':'linear', 'lr':lr} 
+             for lr in [1e-3, 1e-4]]
+
+# 1. define the training and validation function
 def trainval(exp_dict, savedir, args):
     """
     exp_dict: dictionary defining the hyperparameters of the experiment
     savedir: the directory where the experiment will be saved
     args: arguments passed through the command line
     """
-    # 1. Create data loader and model 
+    # 2. Create data loader and model 
     train_loader = he.get_loader(name=exp_dict['dataset'], split='train', 
                                  datadir=os.path.dirname(savedir),
                                  exp_dict=exp_dict)
     model = he.get_model(name=exp_dict['model'], exp_dict=exp_dict)
 
-    # 2. load checkpoint
+    # 3. load checkpoint
     chk_dict = hw.get_checkpoint(savedir)
 
-    # 3. Add main loop
-    for epoch in range(chk_dict['epoch'], 3):
-        # 4. train for one epoch
-        for batch in tqdm.tqdm(train_loader, desc="Training Epoch %d" % epoch):
-            train_dict = model.train_on_batch(batch
+    # 4. Add main loop
+    for epoch in tqdm.tqdm(range(chk_dict['epoch'], 10), 
+                           desc="Running Experiment"):
+        # 5. train for one epoch
+        train_dict = model.train_on_loader(train_loader, epoch=epoch)
 
-        # 5. get and save metrics
+        # 6. get and save metrics
         score_dict = {'epoch':epoch, 'acc': train_dict['train_acc'], 
                       'loss':train_dict['train_loss']}
         chk_dict['score_list'] += [score_dict]
-        hw.save_checkpoint(savedir, score_list=chk_dict['score_list'])
 
-    print('Experiment done')
+    hw.save_checkpoint(savedir, score_list=chk_dict['score_list'])
+    print('Experiment done\n')
 
-# 6. create main
+# 7. create main
 if __name__ == '__main__':
-    # 7. define a list of experiments
-    exp_list = [{'dataset':'syn', 'model':'linear', 'lr':lr} 
-                for lr in [1e-3, 1e-4]]
-
     # 8. Launch experiments using magic command
-    hw.run_wizard(func=trainval, exp_list=exp_list)
+    hw.run_wizard(func=trainval, exp_list=EXP_LIST)
 ```
 
 
--- trainval.py
+++ trainval.py
@@ -1,53 +1,47 @@
-import tqdm, os
+import tqdm
+import os
 
 from haven import haven_examples as he
 from haven import haven_wizard as hw
 
-# 0. define trainval function
+# 0. define a list of experiments
+EXP_LIST = [{'dataset':'syn', 'model':'linear', 'lr':lr} 
+             for lr in [1e-3, 1e-4]]
+
+# 1. define the training and validation function
 def trainval(exp_dict, savedir, args):
     """
     exp_dict: dictionary defining the hyperparameters of the experiment
     savedir: the directory where the experiment will be saved
     args: arguments passed through the command line
     """
-    # 1. Create data loader and model 
+    # 2. Create data loader and model 
     train_loader = he.get_loader(name=exp_dict['dataset'], split='train', 
                                  datadir=os.path.dirname(savedir),
                                  exp_dict=exp_dict)
     model = he.get_model(name=exp_dict['model'], exp_dict=exp_dict)
 
-    # 2. load checkpoint
+    # 3. load checkpoint
     chk_dict = hw.get_checkpoint(savedir)
 
-    # 3. Add main loop
-    for epoch in tqdm.tqdm(range(chk_dict['epoch'], 3), desc="Running experiment"):
-        # 4. train for one epoch
+    # 4. Add main loop
+    for epoch in tqdm.tqdm(range(chk_dict['epoch'], 10), 
+                           desc="Running Experiment"):
+        # 5. train for one epoch
         train_dict = model.train_on_loader(train_loader, epoch=epoch)
 
-        # 5. get and save metrics
+        # 6. get and save metrics
         score_dict = {'epoch':epoch, 'acc': train_dict['train_acc'], 
-                        'loss':train_dict['train_loss']}
+                      'loss':train_dict['train_loss']}
         chk_dict['score_list'] += [score_dict]
-        hw.save_checkpoint(savedir, score_list=chk_dict['score_list'])
 
-    from haven import haven_results as hr
-    rm = hr.ResultManager(exp_list=None, 
-                      savedir_base=os.path.split(savedir)[0], 
-                      filterby_list=None,
-                      verbose=0,
-                      exp_groups=None
-                     )
-    print('Experiment done')
+    hw.save_checkpoint(savedir, score_list=chk_dict['score_list'])
+    print('Experiment done\n')
 
-# 6. create main
+# 7. create main
 if __name__ == '__main__':
-    # 7. define a list of experiments
-    exp_list = [{'dataset':'syn', 'model':'linear', 'lr':lr, 'yours':{'name':'hello', 'lb':3}} 
-                for lr in [1e-3, 1e-4]]
-
     # 8. Launch experiments using magic command
-    hw.run_wizard(func=trainval, 
-                  exp_list=exp_list, 
+    hw.run_wizard(func=trainval, exp_list=EXP_LIST, 
                   job_config={
                             'account_id': os.environ['EAI_ACCOUNT_ID'],
                             'image': 'registry.console.elementai.com/eai.colab/ssh',
