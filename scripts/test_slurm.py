1c8d6b81215d2e31df55c4b72d3ea059b7cdba8c
Sandra Deng <sandra.deng.0520@gmail.com>
2020-11-27 11:42:14 -0800

    task1 done

--- scripts/test_slurm.py
+++ scripts/test_slurm.py
@@ -22,6 +22,9 @@ import numpy as np
 from torch.utils.data import RandomSampler, DataLoader
 print()
 
+import job_configs
+# from haven_utils import file_utils
+
 
 
 
@@ -29,7 +32,18 @@ print()
 # ===========
 def get_job(job_id):
     """Get job information."""
-    return
+    command = "scontrol show job %s" % job_id
+    job_info = ''
+    while True:
+      try:
+        job_info = hu.subprocess_call(command)
+      except:
+        print("scontrol time out and retry now")
+        time.sleep(1)
+        continue
+      break
+    return job_info
+    
 
 def get_jobs(account_id):
     # account_id = hu.subprocess_call('eai account get').split('\n')[-2].split(' ')[0]
@@ -40,6 +54,9 @@ def get_jobs(account_id):
 # ===========
 def kill_job(job_id):
     """Kill a job job until it is dead."""
+    # todo: subprocess_call() time out?
+    kill_command  = "scancel %s" % job_id
+    info = hu.subprocess_call(kill_command)     # no return message after scancel
     return
 import argparse
 
@@ -60,26 +77,43 @@ def get_existing_slurm_job_commands(exp_list, savedir_base):
         
     return existing_job_commands
 
-
-def submit_job(command, savedir, job_config=None):
+def get_job_spec():
     # read slurm setting
     lines = "#! /bin/bash \n"
-    if job_config is not None:
-        lines += "#SBATCH --account=%s \n" % job_config['account_id']
-        lines += "#SBATCH --chdir=%s \n" % job_config['chdir']
-        for key in list(job_config.keys()):
-            lines += "#SBATCH --%s=%s \n" % (key, job_config[key])
+    lines += "#SBATCH --account=%s \n" % job_configs.ACCOUNT_ID
+    for key in list(job_configs.JOB_CONFIG.keys()):
+        lines += "#SBATCH --%s=%s \n" % (key, job_configs.JOB_CONFIG[key])
+    return lines
+
+def submit_job(command, savedir):
+    # read slurm setting
+    lines = "#! /bin/bash \n"
+    # if job_config is not None:
+    #     lines += "#SBATCH --account=%s \n" % job_configs.ACCOUNT_ID
+    #     for key in list(job_config.keys()):
+    #         lines += "#SBATCH --%s=%s \n" % (key, job_config[key])
+    lines += "#SBATCH --account=%s \n" % job_configs.ACCOUNT_ID
+    for key in list(job_configs.JOB_CONFIG.keys()):
+      lines += "#SBATCH --%s=%s \n" % (key, job_configs.JOB_CONFIG[key])
     path_log = os.path.join(savedir, "logs.txt")
     path_err = os.path.join(savedir, "err.txt")
-    command_with_logs = '%s 1>%s 2>%s' % (command, path_log, path_err)
+    lines += "#SBATCH --output=%s \n" % path_log
+    lines += "#SBATCH --error=%s \n" % path_err
     
-    lines += command_with_logs
+    lines += command
 
     file_name = os.path.join(savedir, "bash.sh")
     hu.save_txt(file_name, lines)
     # launch the exp
     submit_command = "sbatch %s" % file_name
-    job_id = hu.subprocess_call(submit_command).split()[-1]
+    while True:
+      try:
+        job_id = hu.subprocess_call(submit_command).split()[-1]
+      except:
+        print("slurm time out and retry now")
+        time.sleep(1)
+        continue
+      break
 
     # save the command and job id in job_dict.json
     job_dict = {
@@ -88,7 +122,7 @@ def submit_job(command, savedir, job_config=None):
     }
     hu.save_json(os.path.join(savedir, "job_dict.json"), job_dict)
 
-    # delete the slurm.sh
+    # delete the bash.sh
     os.remove(file_name)
 
     return job_id
@@ -100,7 +134,7 @@ if __name__ == "__main__":
   Run echo 35 and forward the logs to <savedir>/logs.txt
   """
   command = 'echo 35'
-  savedir = '/home/issamou/shared/results/slurm/example'
+  savedir = '/home/xhdeng/shared/results/test_slurm/example'
   job_id = submit_job(command, savedir)
 
   # task 2 - get job info as dict
@@ -108,40 +142,40 @@ if __name__ == "__main__":
   Get job info as dict and save it as json in the directory specified below
   """
   job_info = get_job(job_id)
-  hu.save_json('/home/issamou/shared/results/slurm/example/job_info.json', job_info)
+  hu.save_json('/home/xhdeng/shared/results/test_slurm/example/job_info.json', job_info)
 
-  # task 3 - kill job
-  """
-  Kill job then gets its info as dict and save it as json in the directory specified below
-  it should say something like CANCELLED
-  """
-  kill_job(job_id)
-  job_info = get_job(job_id)
-  hu.save_json('/home/issamou/shared/results/slurm/example/job_info_dead.json', job_info)
+  # # task 3 - kill job
+  # """
+  # Kill job then gets its info as dict and save it as json in the directory specified below
+  # it should say something like CANCELLED
+  # """
+  # kill_job(job_id)
+  # job_info = get_job(job_id)
+  # hu.save_json('/home/issamou/shared/results/slurm/example/job_info_dead.json', job_info)
   
-  # task 4 - get all jobs from an account as a list
-  """  
-  Get all jobs from an account as a list and save it in directory below
-  """
-  job_info_list = get_jobs(job_id)
-  hu.save_json('/home/issamou/shared/results/slurm/example/job_info_list.json', job_info_list)
+  # # task 4 - get all jobs from an account as a list
+  # """  
+  # Get all jobs from an account as a list and save it in directory below
+  # """
+  # job_info_list = get_jobs(job_id)
+  # hu.save_json('/home/issamou/shared/results/slurm/example/job_info_list.json', job_info_list)
 
-  # task 5 - run 10 jobs using threads
-  """
-  Use thee parallel threads from Haven and run these jobs in parallel
-  """
-  pr = hu.Parallel()
-  command_list = []
+  # # task 5 - run 10 jobs using threads
+  # """
+  # Use thee parallel threads from Haven and run these jobs in parallel
+  # """
+  # pr = hu.Parallel()
+  # command_list = []
 
-  for i in range(1,20):
-    command = 'echo %d' % i
-    savedir = '/home/issamou/shared/results/slurm/example_%d' % i
-    submit_job(command, savedir)
+  # for i in range(1,20):
+  #   command = 'echo %d' % i
+  #   savedir = '/home/issamou/shared/results/slurm/example_%d' % i
+  #   submit_job(command, savedir)
     
-    pr.add(submit_job, command, savedir)
+  #   pr.add(submit_job, command, savedir)
 
-  pr.run()
-  pr.close()
+  # pr.run()
+  # pr.close()
     
 
  
