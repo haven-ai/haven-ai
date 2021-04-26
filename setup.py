ec77f2e6d99146e7d22739b9475e7ef65b326b52
Toolkit User <issam.laradji@gmail.com>
2021-04-26 02:10:27 +0000

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name="haven-ai",
-    version="0.4.6",
+    version="0.5.0",
     description="Manage large-scale experiments",
     url="https://github.com/haven-ai/haven-ai",
     maintainer="Issam Laradji",
