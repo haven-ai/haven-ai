452a17601db3f120d59d6db7fe2a9a600a2dd2ab
Issam H. Laradji <IssamLaradji@users.noreply.github.com>
2021-05-03 12:50:32 -0400

    Update run_with_haven.py

--- run_with_haven.py
+++ run_with_haven.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     # run the trainval
     command = "python trainval.py -e syn -r 1"
     
-    # This command copies a snapshot of the code, saves the logs and errors, and keeps track of the job status
+    # This command copies a snapshot of the code, saves the logs and errors, keeps track of the job status, keeps backup, and ensures one unique command per job
     job = hjb.submit_job(command, savedir_base='../results', job_scheduler='slurm', job_config=SLURM_JOB_CONFIG, reset=True)
     print(job)
